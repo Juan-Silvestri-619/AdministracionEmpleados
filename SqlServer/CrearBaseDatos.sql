28741bab41044eef18f188e63f104a8f9a009d52
Juan-Silvestri-619 <juansilvestri.empleos@gmail.com>
2026-06-10 13:30:11 -0300

    Agrega script de base de datos

---
+++ SqlServer/CrearBaseDatos.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE Pedido;
+
+USE Pedido;
+
+CREATE TABLE Proveedor(
+	id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	nombre VARCHAR(100) NOT NULL,
+	correo VARCHAR(100), 
+	telefono VARCHAR(50) NOT NULL
+
+);
+
+CREATE TABLE Producto(
+	id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	nombre VARCHAR(100) NOT NULL,
+	precio DECIMAL(10,2) NOT NULL CHECK(precio > 0)
+);
+
+CREATE TABLE CabeceraCompra(
+	id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	fecha DATETIME DEFAULT GETDATE(),
+	idProveedor INT NOT NULL,
+
+	FOREIGN KEY (idProveedor) REFERENCES Proveedor(id)
+);
+
+CREATE TABLE DetalleCompra(
+	id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	idCabecera INT NOT NULL,
+	idProducto INT NOT NULL,
+	cantidad INT NOT NULL CHECK(cantidad > 0),
+	precioUnitario DECIMAL(10,2) NOT NULL CHECK(precioUnitario > 0),
+
+	FOREIGN KEY (idCabecera) REFERENCES CabeceraCompra(id),
+	FOREIGN KEY(idProducto) REFERENCES Producto(id)
+);
