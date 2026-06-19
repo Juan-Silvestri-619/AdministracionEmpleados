b39266d6b6169851e09cdd11824efc93a2045887
Juan-Silvestri-619 <juansilvestri.empleos@gmail.com>
2026-06-19 14:37:01 -0300

    Se agrego capa bll y dal de proveedor

---
+++ BLL/ProveedorService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using DAL;
+
+namespace BLL
+{
+    public class ProveedorService
+    {
+        private ProveedorDAL _proveedorDal;
+
+        public ProveedorService()
+        {
+            _proveedorDal = new ProveedorDAL();
+        }
+
+        public Proveedor BuscarId(int id)
+        {
+            return _proveedorDal.BuscarId(id);
+        }
+        public List<Proveedor> ListarProveedores()
+        {
+            return _proveedorDal.ListarProveedores();
+        }
+        public void AgregarProveedor(Proveedor proveedor)
+        {
+            _proveedorDal.AgregarProveedor(proveedor);
+        }
+        public void ActualizarProveedor(Proveedor proveedor)
+        {
+            if (_proveedorDal.BuscarId(proveedor.Id) == null)
+                throw new Exception("No existe el proveedor");
+
+            _proveedorDal.ActualizarProveedor(proveedor);
+        }
+        public void EliminarProveedor(int id)
+        {
+            if (_proveedorDal.BuscarId(id) == null)
+                throw new Exception("No existe el id");
+
+            _proveedorDal.EliminarProveedor(id);
+        }
+    }
+}
---
+++ DAL/ProveedorDAL.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace DAL
+{
+    public class ProveedorDAL
+    {
+        private string _cadenaConexion;
+
+        public ProveedorDAL()
+        {
+            _cadenaConexion = ConfigurationManager.ConnectionStrings["SqlServer"].ConnectionString;
+        }
+
+        public Proveedor BuscarId(int id)
+        {
+            using(SqlConnection conn = new SqlConnection(_cadenaConexion))
+            {
+                conn.Open();
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT id, nombre, correo, telefono FROM Proveedor " +
+                        "WHERE id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using(SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            Proveedor proveedor = new Proveedor(
+                                    Convert.ToInt32(reader["id"]),
+                                    reader["nombre"].ToString(),
+                                    reader["correo"].ToString(),
+                                    reader["telefono"].ToString()
+
+                            );
+                            return proveedor;
+                        }
+                        return null;
+                    }
+                }
+            }
+        }
+        public List<Proveedor> ListarProveedores()
+        {
+            List<Proveedor> proveedores = new List<Proveedor>();
+
+            using(SqlConnection connection = new SqlConnection(_cadenaConexion))
+            {
+                connection.Open();
+
+                using(SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT * FROM Proveedor";
+
+                    using(SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Proveedor proveedor = new Proveedor(
+                                    Convert.ToInt32(reader["id"]),
+                                    reader["nombre"].ToString(),
+                                    reader["correo"].ToString(),
+                                    reader["telefono"].ToString()
+                                );
+
+                            proveedores.Add(proveedor);
+                        }
+                    }
+                }
+            }
+            return proveedores;
+        }
+        public void AgregarProveedor(Proveedor proveedor)
+        {
+            using(SqlConnection connection = new SqlConnection(_cadenaConexion))
+            {
+                connection.Open();
+                using(SqlCommand cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = "INSERT INTO Proveedor (nombre, correo, telefono) " +
+                        "VALUES (@nombre, @correo, @telefono)";
+
+                    cmd.Parameters.AddWithValue("@nombre", proveedor.Nombre);
+                    cmd.Parameters.AddWithValue("@correo", proveedor.Correo);
+                    cmd.Parameters.AddWithValue("@telefono", proveedor.Telefono);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public void ActualizarProveedor(Proveedor proveedor)
+        {
+            using(SqlConnection conn = new SqlConnection(_cadenaConexion))
+            {
+                conn.Open();
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "UPDATE Proveedor " +
+                        "SET nombre = @nombre, correo = @correo, telefono = @telefono " +
+                        "WHERE id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", proveedor.Id);
+                    cmd.Parameters.AddWithValue("@nombre", proveedor.Nombre);
+                    cmd.Parameters.AddWithValue("@correo", proveedor.Correo);
+                    cmd.Parameters.AddWithValue("@telefono", proveedor.Telefono);
+
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+        }
+        public void EliminarProveedor(int id)
+        {
+            using(SqlConnection conn = new SqlConnection(_cadenaConexion))
+            {
+                conn.Open();
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "DELETE FROM Proveedor WHERE id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
